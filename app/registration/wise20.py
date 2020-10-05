b18a79c8bf63b43fbe4de187b6ea91dc1612509f
Timo S. Prinz <t.prinz@campus.tu-berlin.de>
2020-10-05 23:11:26 +0200

    Try fixing caching support

--- app/registration/wise20.py
+++ app/registration/wise20.py
@@ -100,8 +100,6 @@ ABREISE_TIMES = {
     'movormittag': 'Montag Vormittag'
 }
 
-cache = Cache(app)
-
 class Winter20ExkursionenOverwriteForm(FlaskForm):
     exkursion_overwrite = SelectField('Exkursionen Festlegung', choices=EXKURSIONEN_TYPES_FORM)
     submit = SubmitField()
